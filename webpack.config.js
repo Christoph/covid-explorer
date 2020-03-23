cabd2ec05f6d701e73a56a88dd8d214dee2acf51
Chris <christoph.kralj@gmail.com>
2020-03-24 00:44:14 +0100

    Fixed title

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ const when = (condition, config, negativeConfig) =>
   condition ? ensureArray(config) : ensureArray(negativeConfig);
 
 // primary config:
-const title = 'Christoph Kralj';
+const title = 'Covid Explorer';
 const outDir = path.resolve(__dirname, project.platform.output);
 const srcDir = path.resolve(__dirname, 'src');
 const nodeModulesDir = path.resolve(__dirname, 'node_modules');
